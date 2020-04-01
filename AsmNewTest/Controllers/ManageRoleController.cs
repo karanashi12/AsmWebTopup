691152ff315e735219c16856ce999a008a49f01d
ThanhMon99 <52957419+ThanhMon99@users.noreply.github.com>
2020-04-01 23:03:35 +0700

    1

---
+++ AsmNewTest/Controllers/ManageRoleController.cs
@@ -0,0 +1,24 @@
+﻿using AsmNewTest.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AsmNewTest.Controllers
+{
+    public class ManageRoleController : Controller
+    {
+        asmEntities context = new asmEntities();
+
+        // GET: ManageRole
+        public ActionResult Index()
+        {
+            var Model = context.Roles.ToList();
+            return View(Model);
+        }
+
+    }
+}
---
+++ AsmNewTest/Startup.cs
@@ -0,0 +1,22 @@
+﻿using AsmNewTest.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AsmNewTest.Startup))]
+namespace AsmNewTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+
+
+        // In this method we will create default User roles and Admin user for login   
+        
+
+    }
+}
